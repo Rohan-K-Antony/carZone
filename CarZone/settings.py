d6a99074dccec941481f43c20010517354296238
Rohan K Antony <rohan-k.antony@outlook.com>
2023-01-14 13:39:23 +0530

    created pages

--- CarZone/settings.py
+++ CarZone/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = "CarZone.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE":"django.db.backends.postgresql",
+        "NAME":"myCar",
+        "USER":"postgres",
+        "PASSWORD":"1234",
+        "HOST":"localhost"
     }
 }
 
@@ -114,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'CarZone/static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
